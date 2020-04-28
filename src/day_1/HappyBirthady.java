69691e1e649a7c5c03557c0bb62b67b417d431ca
mfunal <64172247+mfunal@users.noreply.github.com>
2020-04-28 14:58:00 -0500

    src/day_1/HappyBirthady.java

--- src/day_1/HappyBirthady.java
+++ src/day_1/HappyBirthady.java
@@ -1,4 +1,7 @@
 package day_1;
 
 public class HappyBirthady {
+    public static void main(String[] args) {
+
+    }
 }
--- src/day_1/addCommand.java
+++ src/day_1/addCommand.java
@@ -1,4 +1,7 @@
 package day_1;
 
 public class addCommand {
+    public static void main(String[] args) {
+
+    }
 }
